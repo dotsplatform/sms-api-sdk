c7e2d23d2fe17de8cbf6458f14f7b0e77584844a
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2022-11-28 19:01:20 +0200

    DEV-14949 added messageType for Account, remamed senderName to smsSenderName, added viberSenderName

--- src/App/DTO/SmsAccountSettingsDTO.php
+++ src/App/DTO/SmsAccountSettingsDTO.php
@@ -7,13 +7,16 @@
 
 namespace Dotsplatform\Sms\DTO;
 
+use App\Services\Sms\DTO\MessageType;
 use Dots\Data\DTO;
 
 class SmsAccountSettingsDTO extends DTO
 {
     protected bool $SMSDisabled = true;
     protected ?string $senderProvider;
-    protected ?string $senderName;
+    protected ?string $smsSenderName;
+    protected ?string $viberSenderName;
+    protected string $messageType = MessageType::SMS;
 
     public function isSMSDisabled(): bool
     {
@@ -25,8 +28,18 @@ class SmsAccountSettingsDTO extends DTO
         return $this->senderProvider;
     }
 
-    public function getSenderName(): ?string
+    public function getSmsSenderName(): ?string
     {
-        return $this->senderName;
+        return $this->smsSenderName;
+    }
+
+    public function getViberSenderName(): ?string
+    {
+        return $this->viberSenderName;
+    }
+
+    public function getMessageType(): string
+    {
+        return $this->messageType;
     }
 }
