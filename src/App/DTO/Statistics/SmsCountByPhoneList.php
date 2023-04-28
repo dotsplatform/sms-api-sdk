9793bd0d7b63d3acf87c30a9b55671398183d817
Yehor Herasymchuk <yehor@dotsplatform.com>
2023-04-28 22:53:40 +0100

    DEV-15509 migrate to laravel 10; add sms count by phone method

---
+++ src/App/DTO/Statistics/SmsCountByPhoneList.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Description of MessagesCountByPhoneList.php
+ * @copyright Copyright (c) DOTSPLATFORM, LLC
+ * @author    Yehor Herasymchuk <yehor@dotsplatform.com>
+ */
+
+namespace Dotsplatform\Sms\DTO\Statistics;
+
+use Illuminate\Support\Collection;
+
+/**
+ * @method SmsCountByPhoneDTO[] all()
+ * @method SmsCountByPhoneDTO|null get($key, $default = null)
+ */
+class SmsCountByPhoneList extends Collection
+{
+    public static function fromArray(array $data): static
+    {
+        return new static(
+            array_map(
+                fn (array $item) => SmsCountByPhoneDTO::fromArray([
+                    'phone' => $item['phone'],
+                    'smsCount' => $item['smsCount'],
+                ]),
+                $data,
+            )
+        );
+    }
+
+}
